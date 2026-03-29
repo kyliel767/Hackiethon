793dcb8b828659cb44adb985be9a57decb23b2d9
jolene7-pixel <jolenelimliqing@gmail.com>
2026-03-29 13:49:03 +1100

    edit

--- pygame_demo/main.py
+++ pygame_demo/main.py
@@ -20,7 +20,7 @@ pygame.display.set_caption("NPC Chat Demo")
 
 
 #load intro background image
-intro_background = pygame.image.load("pygame_demo/assets/intro_background.png")
+intro_background = pygame.image.load("pygame_demo/assets/intro-no-word.png")
 intro_background = pygame.transform.scale(intro_background, (1000, 500))
 
 #load world background image
@@ -74,7 +74,7 @@ white = (255, 255, 255)
 #default font for displaying text
 font = pygame.font.Font(None, 36)
 # Font path for the pixaleted text
-FONT_FILE = "PressStart2P-Regular.ttf"
+# FONT_FILE = "PressStart2P-Regular.ttf"
 #clock for controlling frame rate (i.e. how fast the game loop runs)
 clock = pygame.time.Clock()
 
@@ -91,8 +91,8 @@ player_input = ""
 #------------
 # functions
 #------------
-def get_pixelated_font(size):
-    return pygame.font.Font(FONT_FILE, size)
+#def get_pixelated_font(size):
+    #return pygame.font.Font(FONT_FILE, size)
 
 def handle_player_movement(keys):
     if keys[pygame.K_LEFT]:
@@ -219,33 +219,18 @@ while running:
         # draw intro screen with title
         screen.blit(intro_background, (0, 0))  # background first
         
-        title = get_pixelated_font(40).render("Welcome to the world of\nLittle Red Riding Hood!", True, (211, 100, 17))
-        title_rect = title.get_rect(center=(screen.get_width()//2, screen.get_height()//2 - 100))
+        #title = get_pixelated_font(40).render("Welcome to the world of\nLittle Red Riding Hood!", True, (211, 100, 17))
+        #title_rect = title.get_rect(center=(screen.get_width()//2, screen.get_height()//2 - 100))
 
         #add bounce timing
         bounce_offset = math.sin(pygame.time.get_ticks() * 0.005) * 8
 
         # draw instructions below title
         instructions = font.render("Press ENTER to start.", False, black)
-        instructions_rect = instructions.get_rect(center=(screen.get_width()//2, screen.get_height()//2 + 50 + bounce_offset))
+        instructions_rect = instructions.get_rect(center=(screen.get_width()//2, screen.get_height()//2 + 80 + bounce_offset))
         
-        screen.blit(title, title_rect)
+       #screen.blit(title, title_rect)
         screen.blit(instructions, instructions_rect)
-
-
-        #from previous code
-
-        # draw intro screen with title
-        #screen.fill(black)
-    
-        #title = font.render("Welcome to the world of Little Red Riding Hood!", True, white)
-        #title_rect = title.get_rect(center=(screen.get_width()//2, screen.get_height()//2))
-        
-        # draw instructions below title
-        #instructions = font.render("Press ENTER to start.", True, white)
-        #instructions_rect = instructions.get_rect(center=(screen.get_width()//2, screen.get_height()//2 + 50))
-        #screen.blit(title, title_rect)
-        #screen.blit(instructions, instructions_rect)
 
         
     if game_state == "world":
